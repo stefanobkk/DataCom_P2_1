d5af73fab7d9e034fcdfcfcfa0e9a57ab3be57bc
Thanapatsir@gmail.com <thanapatsir>
2016-11-16 18:57:00 +0700

    add default thread to 6

--- src/main/java/io/muic/dcom/p2/ServerMain.java
+++ src/main/java/io/muic/dcom/p2/ServerMain.java
@@ -9,7 +9,7 @@ import java.util.List;
 
 public class ServerMain {
     static class Config {
-        public static final int DEFAULT_NUM_THREADS = 4;
+        public static final int DEFAULT_NUM_THREADS = 6;
         public static final int DEFAULT_PORT = 7777;
     }
 
